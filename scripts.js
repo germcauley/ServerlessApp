1aff5479eb52476c49d27b24caf6c1754160f612
root <root@LAPTOP-TUFOK6QS.localdomain>
2020-05-12 21:47:55 +0100

    replies working

---
+++ scripts.js
@@ -0,0 +1,154 @@
+//API from API gateway
+var API_ENDPOINT = "https://vswjooboxk.execute-api.eu-west-1.amazonaws.com/dev"
+
+var PARENT_POST_ID ="none"
+
+//Post to webpage
+function newPost(){
+
+	var inputData = {
+		"voice": $('#voiceSelected option:selected').val(),
+        "text" : $('#postText').val(),
+		"user": $('#username').val(),
+		"parentPostID": PARENT_POST_ID
+	};
+
+	$.ajax({
+	      url: API_ENDPOINT,
+	      type: 'POST',
+	      data:  JSON.stringify(inputData)  ,
+	      contentType: 'application/json; charset=utf-8',
+	      success: function (response) {
+					document.getElementById("postIDreturned").textContent="Post ID: " + response;
+					$("#mycontent").empty();
+					$("#posts").empty();
+					renderposts();
+	      },
+	      error: function () {
+	          alert("error");
+	      }
+	  });
+
+	  console.log('POST IS COMPLETED!');
+}
+
+//Send text to API to create new post
+document.getElementById("sayButton").onclick = newPost;
+
+
+//Get all existing record in DB
+// document.getElementById("searchButton").onclick = function(){
+window.onload = renderposts();
+
+
+function renderposts( postId='*'){
+    // var postId = $('#postId').val();
+	// var postId = "*";
+	console.log('Running renderpost func '+postId);
+	$.ajax({
+				url: API_ENDPOINT + '?postId='+postId,
+				type: 'GET',
+				success: function (response) {
+
+					$('#posts tr').slice(1).remove();
+
+	        jQuery.each(response, function(i,data) {
+
+				if(data['parentPostID']=='none'){		
+						console.log('postid is'+postId);		
+						console.log('response is '+i);
+						var player = "<audio controls><source src='" + data['url'] + "' type='audio/mpeg'></audio>"
+
+						if (typeof data['url'] === "undefined") {
+	    				var player = ""
+						}
+						$("#mycontent").append("<div class='row'>\
+								<div class='col-sm-2'>" + data['user'] + "</div>\
+								<div class='col-sm-6'>" + data['text'] + "</div>\
+								<div class='col-sm-3'>" + player + "</div>\
+								<div class='col-sm-1'>\
+								<button id='reply' class='idbutt' value='"+ data['id']+"'>Reply</button>\
+								</div>\
+							  </div>");
+				}
+	        });
+                } 
+                ,
+				error: function () {
+						alert("error");
+				}//Below add onclick event to newly rendered posts
+       		 }).done(function() {
+            	var buttons =document.getElementsByClassName('idbutt')
+            	for (var i=0; i<buttons.length; i++){
+                buttons[i].onclick=function(){
+						var ID = this.value;
+					PARENT_POST_ID = ID;
+					console.log('setting parent post id to :'+PARENT_POST_ID);
+					//pass id into reply function	
+					reply();
+                }
+            }         
+          });
+}
+
+//when user clicks reply, render parent post and children posts
+function reply(){
+
+	console.log('displaying post and replies for post with id' + PARENT_POST_ID)
+	renderposts(PARENT_POST_ID);
+	console.log('running reply function '+PARENT_POST_ID);
+	console.log('this buttons value is'+PARENT_POST_ID);
+	$("#mycontent").empty();
+	$("#posts").empty();
+	renderreply();
+	console.log('posted reply to '+PARENT_POST_ID);
+	
+}
+	
+//make ajax Request for replies
+function renderreply( postId='*'){
+    // var postId = $('#postId').val();
+	// var postId = "*";
+	console.log('Running renderreply '+postId);
+	$.ajax({
+				url: API_ENDPOINT + '?postId='+postId,
+				type: 'GET',
+				success: function (response) {
+
+					$('#posts tr').slice(1).remove();
+
+	        jQuery.each(response, function(i,data) {
+
+				if(data['parentPostID']==PARENT_POST_ID){		
+						console.log('postid is'+postId);		
+						console.log('response is '+i);
+						var player = "<audio controls><source src='" + data['url'] + "' type='audio/mpeg'></audio>"
+
+						if (typeof data['url'] === "undefined") {
+	    				var player = ""
+						}
+						$("#myreplies").append("<div class='row'>\
+								<div class='col-sm-2'>" + data['user'] + "</div>\
+								<div class='col-sm-6'>" + data['text'] + "</div>\
+								<div class='col-sm-3'>" + player + "</div>\
+							  </div>");
+				}
+	        });
+                } 
+                ,
+				error: function () {
+						alert("error");
+				}//Below add onclick event to newly rendered posts
+       		 })
+}
+
+	// make ajax request
+
+document.getElementById("postText").onkeyup = function(){
+	var length = $(postText).val().length;
+	document.getElementById("charCounter").textContent="Characters: " + length;
+}
+
+
+
+
