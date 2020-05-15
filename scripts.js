7482f791799ca25f75a797b3d4a201e1b0a058c4
root <root@LAPTOP-TUFOK6QS.localdomain>
2020-05-15 01:46:57 +0100

    api finsiehd working on frontend

--- scripts.js
+++ scripts.js
@@ -10,6 +10,7 @@ function newPost(){
 		"voice": $('#voiceSelected option:selected').val(),
         "text" : $('#postText').val(),
 		"user": $('#username').val(),
+		"ImageUrl":"https://source.unsplash.com/random",
 		"parentPostID": PARENT_POST_ID
 	};
 
@@ -19,7 +20,7 @@ function newPost(){
 	      data:  JSON.stringify(inputData)  ,
 	      contentType: 'application/json; charset=utf-8',
 	      success: function (response) {
-					document.getElementById("postIDreturned").textContent="Post ID: " + response;
+					// document.getElementById("postIDreturned").textContent="Post ID: " + response;
 					$("#mycontent").empty();
 					$("#posts").empty();
 					renderposts();
@@ -36,24 +37,26 @@ function newPost(){
 document.getElementById("sayButton").onclick = newPost;
 
 
+
 //Get all existing record in DB
 // document.getElementById("searchButton").onclick = function(){
 window.onload = renderposts();
 
+//To render all posts, postid should be *
+//To render a single post use the id as parmeter
+function renderposts( postId='*',parentPostID="none"){
+	
 
-function renderposts( postId='*'){
-    // var postId = $('#postId').val();
-	// var postId = "*";
-	console.log('Running renderpost func '+postId);
+	console.log('Running renderpost func id:'+postId+' parentid: '+parentPostID);
 	$.ajax({
-				url: API_ENDPOINT + '?postId='+postId,
+				url: API_ENDPOINT + '?postId='+postId+'&parentPostID='+parentPostID,
 				type: 'GET',
 				success: function (response) {
-
+					console.log(response);
 					$('#posts tr').slice(1).remove();
 
 	        jQuery.each(response, function(i,data) {
-
+				console.log(response);
 				if(data['parentPostID']=='none'){		
 						console.log('postid is'+postId);		
 						console.log('response is '+i);
@@ -62,14 +65,32 @@ function renderposts( postId='*'){
 						if (typeof data['url'] === "undefined") {
 	    				var player = ""
 						}
-						$("#mycontent").append("<div class='row'>\
-								<div class='col-sm-2'>" + data['user'] + "</div>\
-								<div class='col-sm-6'>" + data['text'] + "</div>\
-								<div class='col-sm-3'>" + player + "</div>\
-								<div class='col-sm-1'>\
-								<button id='reply' class='idbutt' value='"+ data['id']+"'>Reply</button>\
-								</div>\
-							  </div>");
+						// $("#mycontent").append("<div class='row'>\
+						// 		<div id='UserImage' class='col-sm-3'><img src="+ data['ImageUrl']+" alt=''></div>\
+						// 		<div class='col-sm-9'>" + data['text'] + "</div>\
+						// 	  </div>\
+						// 	  <div class='row'>\
+						// 	  	<div class='col-sm-3'>" + data['user'] + "</div>\
+						// 		 <div class='col-sm-7'>" + player + "</div>\
+						// 		 <div class='col-sm-2 btn btn-light' id ='REPLYBTN'><button id='reply' class='idbutt ' value='"+ data['id']+"'>Reply</button></div>\
+						// 	  </div>");
+
+						 $("#mycontent").append("<div class='card'>\
+								<div class='card-header'>"+ data['user'] + "\
+									</div>\
+									<div class='card-body'>\
+										<div class =row>\
+										<div class='col-sm-3'><img src="+ data['ImageUrl']+" alt=''></div>\
+										<div class='col-sm-9'><p class='card-text'>" + data['text'] + "</p></div>\
+										</div>\
+										<div class =row>\
+										<div class='col-sm-3' id ='REPLYBTN'><button id='reply' class='idbutt btn btn-primary ' value='"+ data['id']+"'>Reply</button></div>\
+										<div class='col-sm-7'>" + player + "</div>\
+										<div class='col-sm-2'>\
+										</div>\
+									</div>\
+								</div>");
+						
 				}
 	        });
                 } 
@@ -83,7 +104,6 @@ function renderposts( postId='*'){
                 buttons[i].onclick=function(){
 						var ID = this.value;
 					PARENT_POST_ID = ID;
-					console.log('setting parent post id to :'+PARENT_POST_ID);
 					//pass id into reply function	
 					reply();
                 }
@@ -96,19 +116,21 @@ function reply(){
 
 	console.log('displaying post and replies for post with id' + PARENT_POST_ID)
 	renderposts(PARENT_POST_ID);
-	console.log('running reply function '+PARENT_POST_ID);
-	console.log('this buttons value is'+PARENT_POST_ID);
 	$("#mycontent").empty();
 	$("#posts").empty();
-	renderreply();
+	renderreply(PARENT_POST_ID,PARENT_POST_ID);
+	$("#REPLYBTN").empty();
 	console.log('posted reply to '+PARENT_POST_ID);
-	
+	document.getElementById("REPLYBTN").style.visibility = "hidden";
+
 }
 	
 //make ajax Request for replies
-function renderreply( postId='*'){
-    // var postId = $('#postId').val();
-	// var postId = "*";
+//To render replies postid should be != * and  parentpostiD should!= none
+function renderreply( postId='*',parentPostID='none'){
+    
+
+	
 	console.log('Running renderreply '+postId);
 	$.ajax({
 				url: API_ENDPOINT + '?postId='+postId,
